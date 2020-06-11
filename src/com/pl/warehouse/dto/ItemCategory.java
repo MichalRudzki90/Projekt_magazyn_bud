2e9ce3efc8202ab4ac7dca9e9c9a0c51de36b8de
blazejrudzki <64353273+blazejrudzki@users.noreply.github.com>
2020-06-11 19:37:37 +0200

    Dodałem klasę dziedziczącą po klasie abstrakcyjnej "BaseDto", kategorie produktów( ItemCategory)
    Signed-off-by: blazejrudzki <64353273+blazejrudzki@users.noreply.github.com>

---
+++ src/com/pl/warehouse/dto/ItemCategory.java
@@ -0,0 +1,31 @@
+package com.pl.warehouse.dto;
+
+public class ItemCategory extends BaseDto implements Comparable<ItemCategory> {
+
+	public ItemCategory(Long id, String name) {
+		super(id, name);
+	}
+
+	public ItemCategory(String[] categoryData) {
+		super(Long.valueOf(categoryData[0]), categoryData[1]);
+	}
+
+	public static String headers() {
+		return "| Numer | Nazwa |";
+	}
+
+	public String toCsvData() {
+		return getId() + "," +
+				getName();
+	}
+
+	@Override
+	public String toString() {
+		return "|   " + getId() + "   | " + getName();
+	}
+
+	@Override
+	public int compareTo(ItemCategory o) {
+		return this.getId().compareTo(o.getId());
+	}
+}
